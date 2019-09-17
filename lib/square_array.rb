f99ad8b87cc8c65522d49570566d719756038d41
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-17 21:13:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  counter = 0
-  while counter < array.length do
-    array[counter] = array[counter]**2
+  i = 0
+  while i < array.length do
+    array[i] = array[i]**2
     counter += 1
   end
 end
